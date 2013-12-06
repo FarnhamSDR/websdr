38a4a47148f2363baf85deccafb3967287e49751
Phil Crump <phildcrump@gmail.com>
2013-12-06 15:42:10 +0000

    Deployment setup.

--- start-rtlsdrs.sh
+++ start-rtlsdrs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo killall -9 rtl_tcp
-sudo screen -dmS rtl1 rtl_tcp -d 0 -f 145000000 -s 2048000 -a 127.0.0.1 -p 5901
-sudo screen -dmS rtl2 rtl_tcp -d 1 -f 445500000 -s 2048000 -a 127.0.0.1 -p 5902
+sudo screen -dmS rtl23 rtl_tcp -d 0 -f 1296600000 -s 2048000 -a 127.0.0.1 -p 5901
+sudo screen -dmS rtl432 rtl_tcp -d 1 -f 434000000 -s 2048000 -a 127.0.0.1 -p 5902
+sudo screen -dmS rtl144 rtl_tcp -d 2 -f 145000000 -s 2048000 -a 127.0.0.1 -p 5903
