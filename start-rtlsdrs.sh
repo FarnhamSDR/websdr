0286461c42660cbb0a6544940d9146c6f2377d32
Phil Crump <phildcrump@gmail.com>
2013-12-10 20:20:44 +0000

    Change gains, 70cm freq range in rtl_tcp init script.

--- start-rtlsdrs.sh
+++ start-rtlsdrs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo killall -9 rtl_tcp
-sudo screen -dmS rtl23 rtl_tcp -d 0 -f 1296600000 -s 2048000 -a 127.0.0.1 -p 5901
+sudo screen -dmS rtl23 rtl_tcp -d 0 -f 1296600000 -s 2048000 -g 40 -a 127.0.0.1 -p 5901
 sleep 1;
-sudo screen -dmS rtl432 rtl_tcp -d 1 -f 433800000 -s 2048000 -a 127.0.0.1 -p 5902
+sudo screen -dmS rtl432 rtl_tcp -d 1 -f 433700000 -s 2048000 -g 30 -a 127.0.0.1 -p 5902
 ##sleep 1;
 ##sudo screen -dmS rtl144 rtl_tcp -d 2 -f 145000000 -s 2048000 -a 127.0.0.1 -p 5903
