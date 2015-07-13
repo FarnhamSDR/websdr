3b9b141448a536e92fd2daa479103450916c8ab1
Phil Crump <phil@philcrump.co.uk>
2015-07-13 22:19:12 +0100

    Fix python.

--- ups/Ups.py
+++ ups/Ups.py
@@ -73,7 +73,7 @@ class Ups:
             return False
 
         try:
-            self.batteryNominal = int(self.__getUPSinfo('g'))
+            self.batteryNominal = float(self.__getUPSinfo('g'))
             if self.batteryNominal==24.0:
                 self.batteryVoltage = float(self.__getUPSinfo('B'))/2
             else:
